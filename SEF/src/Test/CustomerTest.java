743ac3bdb8bf0ea202d94f582fb559ba5f0ef587
赵平 <s3630517@student.rmit.edu.au>
2019-04-17 15:14:51 +1000

    Test for Customer class

--- SEF/src/Test/CustomerTest.java
+++ SEF/src/Test/CustomerTest.java
@@ -5,6 +5,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import Class.CreditCard;
 import Class.Customer;
 
 public class CustomerTest extends TestCase {
@@ -12,15 +13,36 @@ public class CustomerTest extends TestCase {
 
 	@Before
 	protected void setUp() throws Exception {
-		customer1 = new Customer("C12345", "Gloria", 3000);
+		
 	}
-
+	
 	@Test
-	public void pointCalculateTest() {
+	public void TestgetPoint() {
+		customer1 = new Customer("C12345", "Gloria", 3000);
 		customer1.calculatePoints(100);
 		assertEquals(customer1.getPoint(), 10);
 	}
 	
+	@Test
+	public void TestcheckDiscount()	{
+		customer1 = new Customer("C12345", "Gloria", 3000);
+		customer1.calculatePoints(200);
+		assertEquals(customer1.checkDiscount(), 5);
+	}
+	
+	@Test
+	public void TestMoreDiscount()	{
+		customer1 = new Customer("C12345", "Gloria", 3000);
+		customer1.calculatePoints(2000);
+		assertEquals(customer1.checkDiscount(), 50);
+	}
+	
+	@Test
+	public void TestaddCard()	{
+		customer1 = new Customer("C12345", "Gloria", 3000);
+		customer1.addCard(new CreditCard(1234543221));
+//		assertEquals(customer1);
+	}
 	@After
 	protected void tearDown() throws Exception {
 	}
