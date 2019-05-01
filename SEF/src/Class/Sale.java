5d4f456408353955aec5bf862273620b7444c4bf
danielongRMIT <49238571+danielongRMIT@users.noreply.github.com>
2019-05-01 15:07:31 +1000

    Update Sale.java

--- SEF/src/Class/Sale.java
+++ SEF/src/Class/Sale.java
@@ -2,18 +2,21 @@ package Class;
 public class Sale {
 	private double profit = 0;
 	private Customer person;
-	private Product[] productID = new Product[50];
-	public Sale(Customer person){
+	private Product[] productCart = new Product[50];
+	public Sale(Customer person) {
 		this.person = person;
-		productID = person.getCart();
+		productCart = person.getCart();
 	}
 	
 	//does the actual sale
 	public void PerformSale(){
 		int i = 0;
-		while(productID[i] != null){
-			productID[i].setProductStock(productID[i].getProductStock()-/*getnumber in cart)*/);
-			profit = profit + (productID[i].getProductPrice()*/*getnumberInCart*/);
+		while(productCart[i] != null){
+			System.out.println("stock was: " + productCart[i].getProductStock());
+			productCart[i].setProductStock(productCart[i].getProductStock()-productCart[i].getNumberInCart());
+			System.out.printf("%d... stock is now: ", productCart[i].getProductStock());
+			profit = profit + (productCart[i].getProductPrice()*productCart[i].getNumberInCart());
+			productCart[i].addConsumption(productCart[i].getNumberInCart());
 			i++;
 		}
 	}
